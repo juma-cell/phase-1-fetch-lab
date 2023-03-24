99da521565c3ba708ed1592a268d226d9cac73f1
Juma Musa <juma.musa@moringaschool.com>
2023-03-24 20:48:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+   return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => renderBooks(data))
+    
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.textContent = book.name;
     main.appendChild(h2);
   });
 }
@@ -15,3 +17,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
